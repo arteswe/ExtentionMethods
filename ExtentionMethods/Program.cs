b8ed6b63c8a5d74efa033638b224f5382fd5bb75
Andrea Manth <andrea.manth-konsult@apoteket.se>
2019-01-19 19:15:41 +0100

    Added delegate function for filtering

--- ExtentionMethods/Program.cs
+++ ExtentionMethods/Program.cs
@@ -1,33 +1,38 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 
 namespace ExtentionMethods
 {
     class Program
     {
+        //smidigt att man anropa en extention istället för att göra beräkningen här eller i en metod
+        //tänk 3:e parts klasser eller t.ex. DataTabel export eller dyl som används på många ställen
+        //readable, går lätt att upptäcka med intellisense (jämför med en method i en annan klass)
+
         static void Main(string[] args)
         {
             Order[] orders =
             {
-                new Order(1, new decimal(23.00)),
-                new Order(1, new decimal(23.10)),
-                new Order(1, new decimal(23.30))
+                new Order(1, new decimal(133.00), "xyz"),
+                new Order(1, new decimal(23.10), "abc"),
+                new Order(1, new decimal(23.30), "def")
             };
 
             var orderGroup = new OrderGroup();
             orderGroup.Orders = orders;
 
-            //smidigt att man anropa en extention istället för att göra beräkningen här eller i en metod
-            //tänk 3:e parts klasser eller t.ex. DataTabel export eller dyl som används på många ställen
-            //readable, går lätt att upptäcka med intellisense (jämför med en method i en annan klass)
-
-            //decimal total = orderGroup.Total();
-
-            Console.Write(orderGroup.Total());
-
             //man kan använda denna metod på alla typer som implementera IEnumerable<Order> som denna enkla array
-            Console.Write(orders.FilterByPrice(30).Total());
+            Console.WriteLine(orderGroup.Total());
+            Console.WriteLine(orders.FilterByPrice(30).Total());
+
+            Func<Order,bool> nameFilter = delegate(Order order)
+            {
+                return order?.orderName()?[0] == 'a';
+
+            };
+            Console.WriteLine(orderGroup.Orders.Filter(nameFilter).Total());
 
             Console.Read();
         }
@@ -37,7 +42,7 @@ namespace ExtentionMethods
     {
         public static string orderName(this Order order)
         {
-            return "no name Order";
+            return order.Name;
         }
 
         public static decimal Total(this IEnumerable<Order> orders)
@@ -60,16 +65,30 @@ namespace ExtentionMethods
             }
         }
 
+        //elegant med en delegat funktion 
+        public static IEnumerable<Order> Filter(this IEnumerable<Order> orders, Func<Order, bool> selector)
+        {
+            foreach (var order in orders)
+            {
+                if (selector(order))
+                {
+                    yield return order;
+                }
+            }
+        }
+
 
     }
     public class Order
     {
+        public string Name { get; set; }
         public decimal Price { get; set; }
         public int Id { get; set; }
 
-        public Order(int id, decimal price)
+        public Order(int id, decimal price, string name)
         {
             Id = id;
+            Name = name;
             Price = price;
         }
     }
